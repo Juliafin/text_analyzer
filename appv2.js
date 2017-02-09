a7429a96b29189cd94b2b653b71e136cac9502a3
Juliafin <juliafinarovsky@gmail.com>
2017-02-08 22:26:48 -0500

    added one last special feature (and an easter egg) to text analyzer

--- appv2.js
+++ appv2.js
@@ -2,28 +2,30 @@
 
 // V 1. function to pull text on submit button click, and return it to a variable as a string
 // V 2. function to convert string to an array, to be analyzed
-// 3. function to convert array into an object
-// 4. function to do word count
-//    a. for each loop sending each element into an object, counting each element in variable "wordcount"
-// 5. unique word count
-//    b. using the for each loop step 2 to send each element into a separate key
-// 6. word length
-//    c. as forEach function runs, store element.length in a variable for each element
-// 7. average sentence length in characters
-//    d. for the original array, designate line breaks (and maybe other things) as sentence enders,
-//     separate each into a separate array and join all the characters and count .length
-// 8. function to inject counter variables for word stats back into the DOM, and remove the .hidden class when done
+// V 3. function to convert array into an object
+// V 4. function to do word count
+//   V a. for each loop sending each element into an object, counting each element in variable "wordcount"
+// V 5. unique word count
+//   V b. using the for each loop step 2 to send each element into a separate key
+// V 6. word length
+//  V  c. as forEach function runs, store element.length in a variable for each element
+// V 7. average sentence length in characters
+//   V  d. for the original array, designate line breaks (and maybe other things) as sentence enders,
+//    V separate each into a separate array and join all the characters and count .length
+// V 8. function to inject counter variables for word stats back into the DOM, and remove the .hidden class when done
 
+var uniqueWordObj = {}; // only global, referenced by uniqueWordObj function
 
-// code definition file
-var uniqueWordObj = {};
+// MAIN FUNCTION
+// Listens for an event on the form
+// On a button click, runs analytics and injects them into the DOM
 
-function formListenPullAnalyze () {
+function formListenPullAnalyze() {
   var allText = "";
-  $('form.js-textform').submit(function(event){
+  $('form.js-textform').submit(function(event) {
     event.preventDefault();
     allText = $('.js-textarea').val();
-    console.log(allText);
+    // console.log(allText);
     // Text as inputed //
     // Text grabbed from textArea
     htmlVarInjector(allText, '.js-raw-text');
@@ -39,7 +41,7 @@ function formListenPullAnalyze () {
     // The elements of the array of words are counted
     var wordArray = arraySplitToWord(allText);
     var wordCount = totalWordCount(wordArray);
-    htmlVarInjector(wordCount, ".js-word-count")
+    htmlVarInjector(wordCount, ".js-word-count");
 
     // Word count without special characters
     // text with no special characters is split into
@@ -83,13 +85,38 @@ function formListenPullAnalyze () {
     // containing all of the unique words. That is fed into the ordered list
     // html creator function
     var uniquewordArr = Object.keys(uniqueWordObj)
-    var uniquewordOL = createOlWithClass(uniquewordArr, "unique words")
+    var uniquewordOL = createOlWithClass(uniquewordArr, "unique words");
     $('.js-unique-word-list').append(uniquewordOL);
 
-  })
-}
 
-// function call
+    // Frequent word list //
+    // Creates a frequent word object using the word array function
+    // Sorts through the keys
+    // Feeds the keys and object into a modified ordered list creator
+    // Injects the ordered list into the DOM
+    var frequentWordObj = frequentWords(wordArray);
+    // console.log(frequentWordObj);
+    var reverseKeys = Object.keys(frequentWordObj).sort(function(a,b) {
+     return frequentWordObj[b]-frequentWordObj[a]
+   });
+   var freqWordHtml = createOlFreqWordWithClass(reverseKeys, 'js-frequent-word-list', frequentWordObj);
+   $('.js-frequent-word-list').append(freqWordHtml);
+    //  console.log(reverseKeys);
+
+     // easter egg
+     if (/(trump)|(Trump)/.test(allText)) {
+      alert("I hope you're not a trump supporter!")
+    }
+   });
+
+    //   frequentWordObj= frequentWordObj.map(function(ele, i, ar){
+    //    frequentWordObj = frequentWordObj[reverseKeys[i]]
+    //  });
+    //    console.log(frequentWordObj);
+  }
+
+
+// Main function call
 
 formListenPullAnalyze();
 
@@ -98,25 +125,27 @@ formListenPullAnalyze();
 function totalCharLen(array) {
   var wordLengthsAr = [];
   var totalLength = 0;
-  array.forEach(function(element){
+  array.forEach(function(element) {
     if (element !== "" && element !== " ") {
-    wordLengthsAr.push(element.length);
-    totalLength+= element.length
+      wordLengthsAr.push(element.length);
+      totalLength += element.length;
     }
   })
   // console.log(wordLengths);
   return totalLength;
 }
 
+
+// do I really have to explain this one?
 function wordAvg(wLen, wCount) {
-  var avg = wLen / wCount;
-  return avg;
-  }
+  return wLen / wCount;
+}
+
 
 // returns the word count of an array
 function totalWordCount(array) {
-  var wordCount = 0
-  array.forEach (function(element){
+  var wordCount = 0;
+  array.forEach(function(element) {
     if (element !== "" && element !== " ") {
       wordCount += 1;
     }
@@ -126,17 +155,11 @@ function totalWordCount(array) {
 }
 
 
-//return true if submitted text is a number AND has no white space
-function hasNumberNoSpc (text) { // method name is confusing
-  // Really pay attention to variable hoisting
+// return true if submitted text is a number AND has no white space
+function hasNumberNoSpc(text) {
 
   var regNum = new RegExp('[0-9]+'); // this matches the string "a9"
   var regNoSpace = new RegExp('^[^\s]+$');
-
-  // indentation represents blocks: functions, while, for, if, switch, etc.
-  // quality syntax: indentation (using tabs vs. spaces, tab length, etc.)
-  //                 variable names
-  //                 appropriate use of functions
 
   if ((regNum.test(text)) === true && (regNoSpace.test(text) === true)) {
     return true;
@@ -144,15 +167,18 @@ function hasNumberNoSpc (text) { // method name is confusing
 
   return false;
 }
-//  "5".LowerCase = 5, "%".toUpperCase = %
-//removes special characters (but leaves spaces) (not including numbers from string)
-function removeSpecial (text) {
+
+
+// "5".LowerCase = 5, "%".toUpperCase = %
+// Filters characters that have the same upper and lower case results
+// removes special characters (but leaves spaces) (not including numbers from string)
+function removeSpecial(text) {
   if (text) {
     var lower = text.toLowerCase();
     var upper = text.toUpperCase();
     var result = "";
-    for(var i=0; i<lower.length; ++i) {
-      if(hasNumberNoSpc(text[i]) || (lower[i] != upper[i]) || (lower[i].trim() === '')) {
+    for (var i = 0; i < lower.length; ++i) {
+      if (hasNumberNoSpc(text[i]) || (lower[i] != upper[i]) || (lower[i].trim() === '')) {
         result += text[i];
       }
     }
@@ -163,41 +189,65 @@ function removeSpecial (text) {
 }
 
 
-
 // converts the text to an array splitting the index at spaces and new line ignores special characters
 function arraySplitToWord(text) {
   var wordSplit = text.split(/ |\n/);
-  console.log(wordSplit);
+  // console.log(wordSplit);
   return wordSplit;
 }
+
 
 // converts the text to an array of sentences:
 // tests whether it is a dictionary sentence or one without punctuation
 // if sentences do not end with standard punctuation, count line breaks
 // outputs an array of sentences
-
 function arraySplitToSent(text) {
   var standardSentEx = new RegExp('/[\.!\?"] /');
   var sentenOut = [];
   if (standardSentEx.test(text) === true) {
     sentenOut = text.split(standardSent);
-    return sentenOut
-    } else {
+    return sentenOut;
+  } else {
     sentenOut = text.split("\n");
     return sentenOut;
-    }
+  }
 }
+
+
+// creates an object of the words occuring most freuqntly
+function frequentWords(wordarr) {
+  var frequentWordsobj = {};
+  wordarr.forEach(function(element){
+    if (frequentWordsobj.hasOwnProperty(element)) {
+      frequentWordsobj[element] +=1
+    } else {
+      frequentWordsobj[element] = 1
+    }
+  });
+  console.log(frequentWordsobj);
+  return frequentWordsobj;
+}
+
+
+// sorts an object in descending order
+function objRevSort(obj) {
+ Object.keys(obj).sort(function(a,b) {
+  obj[a]-obj[b];
+});
+  // console.log (objj);
+}
+
 
 // creates an object out of an array by posting and counting the
 // uniqKeys of the object. returns a count of the unique keys
-// storing the object for (possibly) later use
+// storing the object (with numbericaly increasing values for each key
 function uniqueWordCount(array) {
   var keyCount = 0;
   var id = 0;
-  array.forEach(function(key){
+  array.forEach(function(key) {
     if (uniqueWordObj[key] === undefined) {
-    uniqueWordObj[key] = id++;
-    keyCount += 1;
+      uniqueWordObj[key] = id++;
+      keyCount += 1;
     }
   })
   console.log(keyCount)
@@ -207,14 +257,25 @@ function uniqueWordCount(array) {
 
 // Creates html orderered list elements based on the
 // contents of an array, and outputs the element
-function createOlWithClass (arr, clas) {
-	var OlHtml = ''
-	arr.forEach(function (el) {
-		OlHtml += '<li>' + el + '</li>'
-	});
-	  OlHtml = '<ol class=' + clas + '>' + OlHtml + '</ol>';
-    return OlHtml;
+function createOlWithClass(arr, clas) {
+  var OlHtml = '';
+  arr.forEach(function(el) {
+    OlHtml += '<li>' + el + '</li>'
+  });
+  OlHtml = '<ol class=' + clas + '>' + OlHtml + '</ol>';
+  return OlHtml;
 }
+
+
+function createOlFreqWordWithClass(arr, clas, obj) {
+  var OlHtml = '';
+  arr.forEach(function(el, index, ar) {
+    OlHtml += '<li>' + el + ":" + obj[el] + '</li>'
+  });
+  OlHtml = '<ol class=' + clas + '>' + OlHtml + '</ol>';
+  return OlHtml;
+}
+
 
 // injects variables into the Dom
 function htmlVarInjector(txtvar, clas) {
